dd7054adc6a1ea02f738a265bfe8718bc9c94cda
tiffanychiu97 <t1427t1427@gmail.com>
2023-09-09 20:58:54 +0800

    add login.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <nav>
+  <!-- <nav>
     
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/login">login</router-link> |
     <router-link to="/donate">donate</router-link> |
 
-  </nav>
+  </nav> -->
   <router-view/>
 </template>
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,33 @@
 <template>
- 
-    <h3>this is login.</h3>
-    <Button type="primary">test.  change</Button>
-    <router-view/>
+    <div class="login-section">
+      <h3>LOGIN</h3>
+      <div class="button_group">
+        <button id="register">註冊</button>
+        <button id="login">登入</button>
+      </div>
+      <form action="">
+        <P>請輸入信箱與密碼</P>
+        <input type="email" :value="value" @input="updateValue" placeholder="信箱"/><br>
+        <input type="password" :value="value" @input="updateValue" placeholder="密碼"/>
+        <button class="cybr-btn">登入<span aria-hidden class="cybr-btn__glitch">登入</span></button>
+        <p class="go-register">還沒有帳戶嗎？<span>註冊一個吧！</span></p>
+      </form>
+    </div>
 
   </template>
+  <script>
+  export default {
+  props: {
+    value: String
+  },
+  methods: {
+    updateValue(newValue) {
+      this.$emit('input', newValue);
+    }
+  }
+};
+
+  </script>
+  <style>
+  @import "@/assets/sass/page/_login.scss";
+  </style>
