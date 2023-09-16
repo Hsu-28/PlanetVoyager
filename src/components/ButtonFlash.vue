395910366a517b9ed0554d507aa6538161ff5cd0
shun7220 <ab88227@gmail.com>
2023-09-16 10:43:32 +0800

    完成桌機TeamMembers(除了彈窗)

--- src/components/ButtonFlash.vue
+++ src/components/ButtonFlash.vue
@@ -1,18 +1,17 @@
 
 <template>
     <button class="cybr-btn">
-        {{buttonText}}<span aria-hidden></span>
-        <span aria-hidden class="cybr-btn__glitch">{{buttonText}}</span>
-        <span aria-hidden class="cybr-btn__tag">{{buttonBottomText}}</span>
+        {{ buttonText }}<span aria-hidden></span>
+        <span aria-hidden class="cybr-btn__glitch">{{ buttonText }}</span>
+        <span aria-hidden class="cybr-btn__tag">{{ buttonBottomText }}</span>
     </button>
 </template>
 <style scoped lang="scss">
-  @import "@/assets/sass/component/_buttonFlash.scss";
+@import "@/assets/sass/component/_buttonFlash.scss";
 </style>
 <script>
-export default{
-    props:["buttonText","buttonBottomText"],
+export default {
+    props: ["buttonText", "buttonBottomText"],
     // emits
-    
 }
 </script>
