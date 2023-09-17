562c04cea72e5d3c2631697f5445455fa65a3515
Hsu-28 <svm3g6m4s@gmail.com>
2023-09-17 11:10:38 +0800

    我錯誤

--- src/views/OrderProcess.vue
+++ src/views/OrderProcess.vue
@@ -1,15 +1,20 @@
 <template>
-  <!-- <carousel :items-to-show="3" :wrapAround="true">
-      <slide v-for="slide in 10" :key="slide">
-        {{ slide }}
+  <div>
+    <carousel  :items-to-show="3" :wrapAround="true"  :paginationColor="'#FFF'" class="custom-carousel" :autoplay="3000" >
+      <slide v-for="item in imgs" :key="slide">
+        <div class="cimgs">
+         <img :src=item.slide  alt="輪播圖片">
+        </div>
       </slide>
   
       <template #addons>
         <navigation /> 
-        <pagination />
+        <pagination />  
       </template>
-    </carousel> -->
-  <h1>{{ one }}</h1>
+    </carousel> 
+  </div>
+
+  <!-- <h1>{{ one }}</h1>
   <hr>
   <h1>{{ two }}</h1>
   <hr>
@@ -28,7 +33,7 @@
 
   <div v-for="form in formList">
     <input type="text" v-model="form.name">
-  </div>
+  </div> -->
 </template>
   
 <script>
@@ -42,56 +47,77 @@ export default {
       one: "剩下1",
       two: "剩下2",
       three: "剩下3",
-      totle: [
-        {
-          num: '01',
+      // totle: [
+      //   {
+      //     num: '01',
 
-        },
-        {
-          num: '02',
+      //   },
+      //   {
+      //     num: '02',
 
-        }, {
-          num: '03',
+      //   }, {
+      //     num: '03',
 
-        }, {
-          num: '04',
+      //   }, {
+      //     num: '04',
 
-        }],
-      subContent: [
+      //   }],
+      // subContent: [
 
-      ],
-      amount: 3,
-      currentAmount: 1,
-      formList: []
-    }
-  },
-  computed: {
-    options() {
-      return Array.from({ length: this.amount }, (_, i) => {
-        return {
-          num: i + 1
-        }
-      })
-    },
-  },
-  watch: {
-    currentAmount(nVal) {
-      this.setFormList(nVal)
-    },
-  },
-  methods: {
-    setFormList(num) {
-      this.formList = Array.from({ length: num }, (_, i) => {
-        return {
-          name: '',
-          email: ''
-        }
-      })
-    },
-  },
-  mounted(){
-    this.setFormList(this.currentAmount)
-  },
+      // ],
+      // amount: 3,
+      // currentAmount: 1,
+      // formList: [],
+      
+      imgs: [ {
+            slide: require('@/assets/image/orderProcess/moonc4.svg')
+            },    
+            {
+              slide: require('@/assets/image/orderProcess/moonc2.svg'),
+              
+            },{
+              slide: require('@/assets/image/orderProcess/moonc3.svg'),
+              
+            },{
+              slide: require('@/assets/image/orderProcess/moonc6.svg'),
+              
+            },{
+              slide: require('@/assets/image/orderProcess/moonc5.svg'),
+              
+            },{
+              slide: require('@/assets/image/orderProcess/moonc1.svg'),
+              
+            }
+          ]
+              }
+            },
+  // computed: {
+  //   options() {
+  //     return Array.from({ length: this.amount }, (_, i) => {
+  //       return {
+  //         num: i + 1
+  //       }
+  //     })
+  //   },
+  // },
+  // watch: {
+  //   currentAmount(nVal) {
+  //     this.setFormList(nVal)
+  //   },
+  // },
+  // methods: {
+  //   setFormList(num) {
+  //     this.formList = Array.from({ length: num }, (_, i) => {
+  //       return {
+  //         name: '',
+  //         email: ''
+  //       }
+  //     })
+  //   },
+  // },
+  // mounted(){
+  //   this.setFormList(this.currentAmount)
+  // },
   name: 'App',
   components: {
     Carousel,
@@ -105,9 +131,8 @@ export default {
 
 </script>
 
-<style>
-h1 {
-  font-size: 60px;
-}
+<style scoped lang="scss">
+@import "~@/assets/sass/page/orderProcess.scss";
+
 </style>
   
