38ad06f038c15b6e1258487cea46bceb033b69e7
shun7220 <ab88227@gmail.com>
2023-09-19 20:07:31 +0800

    新增 行前訓練

--- src/router/index.js
+++ src/router/index.js
@@ -96,7 +96,13 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+ 
+   
+  });
+  
+
+
+
 
 export default router
--- src/views/MemberCenter.vue
+++ src/views/MemberCenter.vue
@@ -1,5 +1,5 @@
 <template>
     <h3>member-center change</h3>
     <router-view/>
-      
+     
 </template>
