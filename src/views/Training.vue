c13fa4bfcfcc819fa29a1c4c3d555f10cff8b40e
shun7220 <ab88227@gmail.com>
2023-10-01 16:21:18 +0800

    圖片換jpg

--- src/views/Training.vue
+++ src/views/Training.vue
@@ -78,19 +78,19 @@ export default {
             {
               textTitle: "微重力環境",
               textP: `一旦離開地球的重力井，旅客將體驗到一種飄浮的感覺。\n\n對於初次進入太空的旅客，這種飄浮感可能會造成一些不適，在微重力環境中移動需要使用一種全新的技巧。`,
-              imgSvg: require("@/assets/image/training/flyWater.svg"),
+              imgSvg: require("@/assets/image/training/flyWater.jpg"),
 
 
             },
             {
               textTitle: "宇宙輻射",
               textP: `我們的航天器都配備了專門的屏蔽材料，以最大程度地減少宇宙輻射對旅客的影響。\n\n我們建議旅客在太空中停留的時間不要過長，以確保他們的安全。`,
-              imgSvg: require("@/assets/image/training/radiation.svg"),
+              imgSvg: require("@/assets/image/training/radiation.jpg"),
             },
             {
               textTitle: "太空中的日夜循環",
               textP: `在太空站或太空船上，可能每90分鐘就會經歷一次日出和日落。\n\n這意味著每天將有16次日出和16次日落！這種快速的日夜變化可能會對人的生物鍾產生影響。`,
-              imgSvg: require("@/assets/image/training/sun.svg"),
+              imgSvg: require("@/assets/image/training/sun.jpg"),
             },
             // {
             //   textTitle: "太空真空與溫度變化",
@@ -108,21 +108,21 @@ export default {
             {
               textTitle: "個人衛生與健康管理",
               textP: `在太空環境中，沐浴和洗手都需要使用特殊的方法。\n\n此外，太空中的微重力環境會對人體骨骼和肌肉產生影響，所以每天的運動是必要的日常活動。`,
-              imgSvg: require("@/assets/image/training/bath.svg"),
+              imgSvg: require("@/assets/image/training/bath.jpg"),
 
 
             },
             {
               textTitle: "飲食系統",
               textP: `旅客將了解太空船的資源循環系系統是如何使用的，\n\n如何在太空中用餐、節約水資源，並了解這些系統對於長期太空旅行的重要性。`,
-              imgSvg: require("@/assets/image/training/resource.svg"),
+              imgSvg: require("@/assets/image/training/resource.jpg"),
 
             },
 
             {
               textTitle: "休息和睡眠",
               textP: `在太空中睡覺是一種獨特的體驗，由於沒有地心引力，旅客將在太空袋中飄浮地睡覺。\n\n太空站內部的光線會模擬地球上的日夜循環，幫助維持旅客的生物鍾。`,
-              imgSvg: require("@/assets/image/training/sleep.svg"),
+              imgSvg: require("@/assets/image/training/sleep.jpg"),
 
             },
             // {
@@ -154,14 +154,14 @@ export default {
             {
               textTitle: "太空船的重要設備",
               textP: `認識太空站內的精密和關鍵的儀器，並告知其重要性,\n\n除非得到許可或在專業人員的指導下，否則請避免觸碰任何儀器或控制面板。誤操作可能導致嚴重的後果。`,
-              imgSvg: require("@/assets/image/training/equipment.svg"),
+              imgSvg: require("@/assets/image/training/equipment.jpg"),
 
             },
 
             {
               textTitle: "緊急撤離程序",
               textP: `如果太空站發生緊急情況，如火災或氧氣泄露，旅客應迅速而冷靜地遵循緊急撤離程序。\n\n每位旅客在太空旅行前都會接受撤離訓練，熟悉所有緊急出口和救生艙的位置`,
-              imgSvg: require("@/assets/image/training/exit.svg"),
+              imgSvg: require("@/assets/image/training/exit.jpg"),
 
             },
             // {
