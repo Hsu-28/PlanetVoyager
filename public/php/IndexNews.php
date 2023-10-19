381bb933278bfe364788f86fea6b85befb2a417c
MegaraWan <chaioyazhou@gmail.com>
2023-10-19 18:49:10 +0800

    首頁 bug 解除

--- public/php/IndexNews.php
+++ public/php/IndexNews.php
@@ -5,8 +5,8 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Content-Type: application/json"); 
-require_once("../../connect_chd103g3.php");
-
+require_once("../../../connect_chd103g3.php");
+            
 
 
 // 下sql指令
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -259,6 +259,7 @@ export default {
                 },
             ],
             activeId: 1,
+            
             news: [],
 
             vehicles: [
